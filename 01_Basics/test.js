86fe5987ba4300c3b3965c20ae6741ce895654a8
ParthkumarLakhani <132048357+ParthkumarLakhani@users.noreply.github.com>
2024-03-28 17:21:04 +0000

    config done and variable

---
+++ 01_Basics/test.js
@@ -0,0 +1,27 @@
+
+//Declare variable 
+
+firstName = "Parth"
+firstName = "Parth_Lakhani"
+console.log(firstName);
+
+
+const id = 180303108
+console.log(id);
+
+
+let assign;
+assign = 22
+console.log(assign);
+
+var globalOld = "now we are not use that much"
+var globalOld = "Use Instead var, Let"
+console.log(globalOld);
+
+
+//4 ways to Declare variable
+//auto 
+//var - no scope(functional scope), redeclare, reasssign, hoisted, 
+//let - Block scope, can't redeclare, reassign, can't hoisted,
+//const - Block scope,  can't redeclare, can't reassign, can't hoisted,
+
